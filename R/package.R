56ff3c5c2529246d441f6248e9075dd720ef77c6
Saisaran17 <113111560+Saisaran17@users.noreply.github.com>
2022-11-25 14:15:39 +0530

    Add files via upload

---
+++ R/package.R
@@ -0,0 +1,13 @@
+# sum of n numbers
+#
+# This is an example function named 'sum_of_numbers'
+# This returns the sum of n numbers
+#
+
+
+sum_of_numbers <- function(num) {
+
+    sum = (num * (num + 1)) / 2;
+    return(sum)
+
+}
